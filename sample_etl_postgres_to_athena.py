e954a23dce662f1dc8ca28b8fe44ba89b92c7cdd
Brian Davis <davisbriant@gmail.com>
2024-09-28 20:33:16 -0700

    add sql alt version

--- sample_etl_postgres_to_athena.py
+++ sample_etl_postgres_to_athena.py
@@ -45,8 +45,10 @@ zips_df = zips_query.toDF()
 zips_df.printSchema()
 # The dataframe schema contains one column dma_id of type integer and another column zips of type array. We will create a new row for every zip code in the array of zips per dma_id.
 zips = zips_df.select("dma_id", explode_outer("zips").alias("zip"))
-zips.printSchema()
+# Because some zip codes are members of multiple dmas, we de-duplicate the dataframe rows so there is a 1:1 ratio between dma_id and zip.
+zips = zips.dropDuplicates(["dma_id"])
 # The new schema of the dataframe contains one column dma_id of type integer and another column zip of type string.
+zips.printSchema()
 # now we will connect to postgres and import a table containing dma metadata
 dmas_query = glueContext.create_dynamic_frame.from_options ( 
  connection_type="postgresql",
@@ -63,8 +65,6 @@ dmas_df.printSchema()
 # The dma metadata dataframe is joined with the dma zips dataframe using key dma_id.
 dmas = dmas_df.join(zips, ["dma_id"], "right")
 dmas.printSchema()
-# Because some zip codes are members of multiple dmas, we de-duplicate the dataframe rows so there is a 1:1 ratio between dma_id and zip.
-dmas = dmas.dropDuplicates(["zip"])
 # Now we will connect to postgres and import a table containing data on home insurance policies.
 policies_query = glueContext.create_dynamic_frame.from_options ( 
  connection_type="postgresql",
@@ -89,6 +89,28 @@ policies_bydma.printSchema()
     .option("path", f"s3://{output_bucket}/glue/postgres/policiesbydma/")
     .format("parquet")
     .saveAsTable("default.glue_test_postgres_policiesbydma")
+)
+# The data visualization team can now design dashboards that analyze policy volumes broken down by customer cohort criteria including dma.
+# now let's do that again but this time we'll let postgres handle the unnesting, deplication, and joins logic
+policiesbydma_query = "with dmasperzip as (select dma_id, unnest(zips) as zip2 from zipsindmas ), dmaperzip as (select *, row_number() over (partition by zip2 order by dma_id desc) as row_num from dmasperzip ), dmaperzip_metadata as (select a.zip2, b.* from dmaperzip a left join dmametadata b on a.dma_id = b.dma_id where a.row_num = 1 ) select a.*, b.* from dmaperzip_metadata a right join dm_policies_extended b on a.zip2 = b.zip" 
+policiesbydma = glueContext.create_dynamic_frame.from_options ( 
+ connection_type="postgresql",
+ connection_options = {
+ "url": db_url,
+ "user": db_username,
+ "password": db_password,
+ "dbtable": "public.zipsindmas",
+ "sampleQuery":policiesbydma_query
+ }
+)
+# The resulting dataframe is saved as a parquet table in the awsdatacatalog so it can be queried in Athena.
+
+(policies_bydma
+    .write
+    .mode("overwrite")
+    .option("path", f"s3://{output_bucket}/glue/postgres/policiesbydma_alt/")
+    .format("parquet")
+    .saveAsTable("default.glue_test_postgres_policiesbydma_alt")
 )
 # The data visualization team can now design dashboards that analyze policy volumes broken down by customer cohort criteria including dma.
 job.commit()
